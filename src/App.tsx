4337b7c8c6c73cacb470ac6862d8a9cc2a72cf62
pavelbekarev <bekarev.ps@gmail.com>
2025-05-10 18:36:45 +0500

    Добавил webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Header } from "./shared/Header";
 import "./App.scss";
-import { Article } from "./shared/Article";
+import { Article } from "./shared/Article/ui/index";
 
 function App() {
   return (
